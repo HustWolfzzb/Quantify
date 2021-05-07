d452286771ebe9ed1c7e99c4c37777b5519a331f
HustWolfzzb <1184827350@qq.com>
2021-05-07 11:53:06 +0800

    clear

--- HaiTong.py
+++ HaiTong.py
@@ -33,11 +33,10 @@ for x in codes:
     open_buy[x] = []
 
 
+
+
 def get_all_price(codes = codes):
     return qo.stocks(codes)
-
-price_close = get_all_price()
-price_close = [price_close[x]['close'] for x in codes]
 
 
 class User():
@@ -134,167 +133,6 @@ class Trade():
 def get_Account():
     return User(user)
 
-def open_grid_buy():
-    pos = user.position
-    keyong_money = user.balance['可用金额']
-    keyong_amount = []
-    for code in codes:
-        for p in pos:
-            if p['证券代码'] == code:
-                keyong_amount.append(p['可用余额'])
-    print(keyong_amount)
-    # price_close = [1.431, 1.196, 1.814, 1.312]
-    # keyong_money = 20000
-    # keyong_amount = [8100, 3600, 5500, 200]
-#     #开盘的时候挂卖盘
-    for code_idx in range(len(codes)):
-        price =  price_close[code_idx]
-        ka = keyong_amount[code_idx]
-        for i in range(4):
-            try:
-                new_amount = pow(2, i) * 100
-                for j in range(i+1):
-                    new_amount = pow(2,i) * 100
-                    if ka>new_amount:
-                        price += change_[code_idx]
-                        price = round(price, 3)
-                        open_sell[codes[code_idx]].append(user.sell(codes[code_idx], price, new_amount))
-                        print("Sell %s, %s, %s, %s"%(names[code_idx], price, new_amount , round(price/price_close[code_idx],3) ))
-                        time.sleep(1)
-                        ka -= new_amount
-                    elif ka>100:
-                        price += change_[code_idx]
-                        price = round(price, 3)
-                        # open_sell[codes[code_idx]].append(user.sell(codes[code_idx], price , ka-100))
-                        print("Sell %s, %s, %s, %s"%(names[code_idx], price, ka-100 , round(price/price_close[code_idx],3) ))
-                        ka = 100
-            except Exception as e:
-                if str(e).find("客户股票不足"):
-                    print("客户%s股票不足"%names[code_idx])
-#     # 开盘的时候挂买盘
-    km = keyong_money
-    for code_idx in range(len(codes)):
-        price = price_close[code_idx]
-        for i in range(3):
-            new_amount = pow(2, i) * 100
-            for j in range(i + 1):
-                if km > (price-0.004) * new_amount:
-                    price -= change_[code_idx]
-                    price = round(price, 3)
-                    #  open_buy.append(user.sell(codes[code_idx], price , new_amount))
-                    print("Buy %s, %s, %s, %s" % (
-                    names[code_idx], price, pow(2, i) * 100, round(price / price_close[code_idx], 3)))
-                    km -= new_amount * price
-                elif km > 100 * (price-0.004):
-                    amount = int(km / ((price - 0.004)*100))
-                    price -= change_[code_idx]
-                    price = round(price, 3)
-                    # open_buy.append(user.sell(codes[code_idx], price , ka-100))
-                    print("Buy %s, %s, %s, %s" % (names[code_idx], price, amount, round(price / price_close[code_idx], 3)))
-                    km -= amount * price
-
-
-def get_amount(now, close, gap):
-    if now > close:
-        return operate[int((now - close)//gap)]
-    else:
-        return operate[int((close - now)//gap)]
-
-#网格交易法
-def spy_price():
-    start = True
-    # start = False
-    p = get_all_price()
-    price_close = [p[x]['now'] for x in codes]
-    operate_price = [x for x in price_close]
-    print(price_close)
-    # buy_amount = [[] for x in range(len(codes))]
-    # sell_amount =[[] for x in range(len(codes))]
-    # price = [1.431, 1.429, 1.427, 1.425, 1.423, 1.421, 1.419, 1.417, 1.415, 1.413, 1.411, 1.409, 1.407,
-    #           1.405, 1.408, 1.411, 1.414, 1.417, 1.42, 1.423, 1.426, 1.429, 1.432,
-    #           1.435, 1.433, 1.431, 1.429, 1.427, 1.425, 1.423, 1.421, 1.419, 1.417,
-    #           1.415, 1.417, 1.419, 1.421,
-    #           1.421, 1.416, 1.411, 1.406, 1.401, 1.396,
-    #           1.397, 1.398, 1.399, 1.4, 1.401, 1.402, 1.403, 1.404, 1.405, 1.406, 1.407, 1.408, 1.409]
-    # cou = 0
-    # price_close = [1.431, 1.196, 1.814, 1.312]
-
-    # use = 5500
-    # money = operate_price[code_idx * use
-    sell_ids = {}
-    buy_ids = {}
-    buy_amount = 100
-    sell_amount = 100
-    while True:
-        time.sleep(1)
-        # if cou>=len(price):
-        #     break
-        p = get_all_price()
-        price_now = [p[x]['now'] for x in codes]
-        # price_now = [price[cou],]
-        # cou += 1
-        # op_amount = 0
-        for code_idx in range(len(codes)):
-        # for code_idx in range(len(codes[0:1])):
-            close_price = price_close[code_idx]
-            code = codes[code_idx]
-            now_price = price_now[code_idx]
-            gap = change_[code_idx]
-            try:
-                if start:
-                    start = False
-                    # print("挂 Sell %s, %s, %s, %s" % (names[code_idx], close_price + gap, amount , round(now_price / price_close[code_idx], 3)))
-                    buy = Trade(code,  close_price - gap, buy_amount, 'b')
-                    buy_ids[code] = buy
-                    # print("挂 Buy %s, %s, %s, %s" % (names[code_idx], close_price - gap, amount , round(now_price / price_close[code_idx], 3)))
-                    sell = Trade(code, close_price + gap, sell_amount, 's')
-                    sell_ids[code] = sell
-                    # op_amount = 100
-
-                if now_price >= operate_price[code_idx] + gap:
-                    if sell_ids.get(code) == None:
-                        sell_ids[code] = Trade(code, now_price, sell_amount, 's')
-                    else:
-                        buy_amount = get_amount(now_price, close_price, gap)
-                        sell_amount = get_amount(now_price + gap, close_price, gap)
-                        # op_amount = amount
-                        operate_price[code_idx] = now_price
-                        sell_ids[code_idx] = user.sell(codes[code_idx], round(now_price + gap, 3), sell_amount)[
-                            'entrust_no']
-                        print("挂 Sell %s, %s, %s, %s" % (names[code_idx], round(now_price + gap, 3), buy_amount,
-                                                         round(now_price / price_close[code_idx], 3)))
-                        if buy_ids[code_idx] != '':
-                            print("撤买单%s" % sell_ids[code_idx])
-                            user.cancel_entrust(buy_ids[code_idx])
-                        buy_ids[code_idx] = user.buy(codes[code_idx], round(now_price - gap, 3), buy_amount)[
-                            'entrust_no']
-                        print("挂 Buy %s, %s, %s, %s" % (names[code_idx], round(now_price - gap, 3), buy_amount,
-                                                        round(now_price / price_close[code_idx], 3)))
-                    # use -= op_amount
-                    print("%s 价格：%s,的卖单 成交！"%(names[code_idx],operate_price[code_idx] + gap))
-
-
-                elif now_price <= operate_price[code_idx] - gap:
-                    if buy_ids.get(code) == None:
-                        buy_ids[code] = Trade(code, now_price, buy_amount, 'b')
-                    # use += op_amount
-                    print("%s 价格：%s,的买单 成交！"%(names[code_idx],operate_price[code_idx] - gap))
-                    buy_amount = get_amount(now_price - gap, close_price, gap)
-                    sell_amount = get_amount(now_price, close_price, gap)
-                    # op_amount = amount
-                    operate_price[code_idx] = now_price
-                    buy_ids[code_idx] = user.buy(codes[code_idx],  round(now_price - gap, 3), buy_amount)['entrust_no']
-                    print("挂 Buy %s, %s, %s, %s" % (names[code_idx], round(now_price - gap, 3), buy_amount, round(now_price / price_close[code_idx], 3)))
-                    if sell_ids[code_idx] != '':
-                        print("撤%s卖单%s"%sell_ids[code_idx])
-                        user.cancel_entrust(sell_ids[code_idx])
-                    sell_ids[code_idx] = user.sell(codes[code_idx],  round(now_price + gap, 3), sell_amount)['entrust_no']
-                    print("挂 Sell %s, %s, %s, %s" %(names[code_idx], round(now_price + gap, 3), buy_amount, round(now_price / price_close[code_idx], 3)))
-            except KeyError as e:
-                print(e)
-            except Exception as e:
-                if str(e).find('客户股票不足'):
-                    print('客户%s股票不足'%names[code_idx])
 
 def spy_on_etf():
     # start = False
@@ -332,12 +170,12 @@ def spy_on_etf():
                 buyer = buyers[i]
                 seller = sellers[i]
                 operate_price = operate_prices[i]
-                if price_now < operate_price * (1 - gap):
+                if price_now < operate_price - gap:
                     buy_price = round(operate_price * (1 - gap),3)
                     buy_amount = buy_amount
                     buy_id = buyer.trade(code, buy_price, buy_amount, 'b')
                     operate_prices[i] = buy_price
-                if price_now > operate_price * (1 + gap):
+                if price_now > operate_price + gap:
                     sell_price = round(operate_price * (1 + gap), 3)
                     sell_amount = sell_amount
                     sell_id = seller.trade(code, sell_price, sell_amount, 's')
